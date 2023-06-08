fc5d41ca4ed125773e34dd3f68017f374c2f77dc
Nikita-Bhoyar <nikitabhoyar99@gmail.com>
2023-06-09 01:02:46 +0530

    Add files via upload

---
+++ Bankaccount.cs
@@ -0,0 +1,91 @@
+﻿class BankAccount
+{
+    private static int accountCounter = 0;
+   // private IEnumerable<BankAccount> accounts;
+
+    static decimal Balance { get; set; }
+    public static string AccountNumber { get; }
+    public static string AccountHolderName { get; set; }
+    public static string DisplayAccountDetails { get; set; }
+
+    public BankAccount(string accountHolderName, decimal initialBalance)
+    {
+        AccountHolderName = accountHolderName;
+        Balance = initialBalance;     
+    }
+
+    public BankAccount()
+    {
+        
+    }
+    protected virtual string GenerateAccountNumber()
+    {
+        accountCounter++;
+        return "ACC" + accountCounter.ToString("D4");
+    }
+    public  void CreateAccount()
+    {
+
+        List<BankAccount> accounts = new List<BankAccount>();
+        Console.Write("Enter account holder name: ");
+        string accountHolderName = Console.ReadLine();
+        Console.Write("Enter initial balance: ");
+        decimal initialBalance = decimal.Parse(Console.ReadLine());
+       
+        BankAccount account = new BankAccount(accountHolderName, initialBalance);
+        accounts.Add(account);
+
+        Random random = new Random();
+        int randomNumber = random.Next(1000, 9999); // Generate a random 4-digit number
+
+        string accountNumber = "ACC" + accountCounter.ToString("D4") + randomNumber.ToString();
+
+        Console.WriteLine("Your Account Number is : $ " + accountNumber);
+
+        Console.WriteLine("Account created successfully!");
+    }
+
+    public static void DisplayAccountHolderName()
+    {
+        Console.WriteLine("Account Holder Name: " + AccountHolderName);
+    }
+
+    public static void DisplayBalance()
+    {
+        Console.WriteLine("Balance: $" + Balance);
+    }
+
+    public static void DepositMoney(decimal amount)
+    {
+        if (amount > 0)
+        {
+            Balance += amount;
+            Console.WriteLine("Successfully deposited $" + amount);
+        }
+        else
+        {
+            Console.WriteLine("Invalid amount! Deposit failed.");
+        }
+    }
+
+    public static void WithdrawMoney(decimal amount)
+    {
+        if (amount > 0 && amount <= Balance)
+        {
+            Balance -= amount;
+            Console.WriteLine("Successfully withdrawn $" + amount);
+        }
+        else
+        {
+            Console.WriteLine("Invalid amount or insufficient balance! Withdrawal failed.");
+        }
+    }
+
+    public static void DisplayAccountDetails()
+    {
+        Console.WriteLine("Account Number: " + AccountNumber);
+        Console.WriteLine("Account Holder Name: " + AccountHolderName);
+        Console.WriteLine("Balance: " + Balance);
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿class program
+{
+    private static object access;
+    public static string AccountHolderName { get; set; }
+    public static decimal Balance { get; private set; }
+
+    public static void Main(string[] args)
+    {
+        BankAccount account = new BankAccount();
+
+        bool isRunning = true;
+       
+        while (isRunning)
+        {
+            Console.WriteLine("1. Create Account");
+            Console.WriteLine("2. Display Account Holder Name");
+            Console.WriteLine("3. Display Balance");
+            Console.WriteLine("4. Deposit Money");
+            Console.WriteLine("5. Withdraw Money");
+            Console.WriteLine("6. DisplayAllAccounts");
+            Console.WriteLine("7. Exit");
+            Console.Write("Enter your choice: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    account.CreateAccount();
+                    break;
+                case 2:
+                    account.DisplayAccountDetails();
+                    break;
+                case 3:
+                    account.DisplayBalance();
+                    break;
+                case 4:
+                    Console.Write("Enter the account number: ");
+                    string accountNumber = (Console.ReadLine());
+                    Console.Write("Enter the amount to deposit: ");
+                    decimal depositAmount = decimal.Parse(Console.ReadLine());
+                    DepositMoney(depositAmount);
+                    break;
+                case 5:
+                    //Console.Write("Enter the account number: ");
+                    //string accountNumber = (Console.ReadLine());
+                    Console.Write("Enter the amount to withdraw: ");
+                    decimal withdrawAmount = decimal.Parse(Console.ReadLine());
+                    WithdrawMoney(withdrawAmount);
+                    break;
+                case 6:
+                    DisplayAccountDetails();
+                    break;
+                case 8:
+                    isRunning = false;
+                    Console.WriteLine("Exiting the program...");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice! Please try again.");
+                    break;
+            }
+            Console.WriteLine();
+        }
+    }
+
+  
+}
